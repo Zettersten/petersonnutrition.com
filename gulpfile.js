d305051236ff13d6dbf51010eadd3b6386e8983d
Erik Zettersten <erik@zettersten.com>
2020-05-27 23:29:08 -0400

    adds simple html include, adds build and clean jobs, and deploys stuff

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,11 @@ var webserver = require("gulp-webserver");
 var gutil = require("gulp-util");
 var ftp = require("vinyl-ftp");
 var env = require("gulp-env");
+var include = require("gulp-html-tag-include");
+var clean = require("gulp-clean");
 
 gulp.task("dev", function() {
-    gulp.src("./src")
+    gulp.src("./dist")
         .pipe(
             webserver({
                 livereload: true,
@@ -14,7 +16,28 @@ gulp.task("dev", function() {
         );
 });
 
-gulp.task("deploy", function() {
+gulp.task("html-include", function() {
+    return gulp.src("./src/**/*.html")
+        .pipe(include())
+        .pipe(gulp.dest("./dist/"));
+});
+
+gulp.task("build", function() {
+    return gulp.src("./src/**/!(*.html)")
+        .pipe(gulp.dest("./dist/"));
+});
+
+gulp.task("clean", function() {
+    return gulp.src("./dist", { read: false })
+        .pipe(clean());
+});
+
+gulp.task("default", gulp.parallel("html-include", "build", "dev", function() {
+    gulp.watch("./src/**/*.html", gulp.series("html-include"));
+    gulp.watch("./src/**/!(*.html)", gulp.series("build"));
+}));
+
+gulp.task("deploy", gulp.series("clean", "html-include", "build", function() {
 
     env({ file: ".env.json" });
 
@@ -22,15 +45,12 @@ gulp.task("deploy", function() {
         host: process.env.FTP_HOST,
         user: process.env.FTP_USER,
         password: process.env.FTP_PASSWORD,
-        parallel: 10,
+        parallel: 2,
         log: gutil.log
     });
 
-    // using base = "." will transfer everything to /public_html correctly
-    // turn off buffering in gulp.src for best performance
-
-    return gulp.src("README.md", { base: ".", buffer: false })
-        .pipe(conn.newer("/src")) // only upload newer files
+    return gulp.src("./dist/**/*", { base: "./dist", buffer: false })
+        .pipe(conn.newer("/dist")) // only upload newer files
         .pipe(conn.dest("/public_html/PNF"));
 
-});
+}));
