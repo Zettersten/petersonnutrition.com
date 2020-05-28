ad80f4eabfc397a1080637287eac4a119c28973c
Erik Zettersten <erik@zettersten.com>
2020-05-27 21:38:30 -0400

    adds contents

---
+++ contact.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Peterson Nutrition and Fitness Richmond Virginia :: Programs for individuals and groups</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta http-equiv="content-language" content="en" />
+<link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
+<meta name="generator" content="One James [ www.1james.com ]" />
+	<meta name="author" content="One James [ www.1james.com ]" />
+    <meta name="Keywords" content="" />
+	<meta name="Description" content="" />
+	<link rel="stylesheet" href="all.css" type="text/css" media="all" />
+	<style type="text/css"> 
+	<!-- 
+	#nav06{background:transparent url(i/nav.gif) -788px -840px no-repeat}
+	--> 
+	</style>
+      <link rel="stylesheet" type="text/css" href="gbcf-v3/files/themes/default.css" media="screen" />
+	 <!--[if IE]>	
+	  <script type="text/javascript" src="gbcf-v3/files/focus.js"></script>	
+	 <![endif]-->
+
+<!--[if lte IE 7]><style type="text/css">#header {padding:20px 10px 3px}</style><![endif]-->
+<!--[if IE 5]><style type="text/css">#container {width:910px}</style><![endif]-->
+
+</head>
+
+<body>
+
+
+<div id="container">
+
+<img src="i/nav.png" alt="Menu" border="0" usemap="#Menu" />
+<map name="Menu" id="Menu">
+    <area shape="rect" coords="17,7,334,123" href="index.html" alt="HOME" />
+    <area shape="poly" coords="386,134,441,134,467,106,460,89,441,82" href="index.html" alt="HOME" />
+    <area shape="poly" coords="495,134,440,134,499,77,515,79,524,89,527,101" href="about.html" alt="ABOUT" />
+    <area shape="poly" coords="549,133,496,134,563,67,578,68,588,78,587,94" href="services.html" alt="SERVICES" />
+    <area shape="poly" coords="602,134,549,133,627,57,643,57,653,69,652,84" href="news.html" />
+    <area shape="poly" coords="656,134,603,134,700,37,714,36,728,49,726,66" href="faq.html" alt="FAQ/RESOURCES" />
+    <area shape="poly" coords="710,134,658,134,725,68,738,69,749,81,749,95" href="contact.php" alt="CONTACT" />
+</map>
+        
+	<div id="content">    
+
+		<div id="sidebar">
+		<h3 class="sidehead">CONTACT</h3>
+		<p>Elisabeth  Peterson, RD, CEDRD<br />
+		  Medical Nutrition Therapist
+		    <br />
+	      AFAA Certified Personal Trainer </p>
+		<p>(p) 804-440-3110</p>
+		<p>2200 Pump Rd., Suite 220 Richmond, VA 23233</p>
+		<img src="i/pnfmap.gif" width="219" height="300" />
+		<p><a href="http://maps.google.com/maps?client=safari&amp;oe=UTF-8&amp;ie=UTF8&amp;cid=0,0,14661453596443134065&amp;fb=1&amp;hnear=Fort+Collins,+CO&amp;gl=us&amp;daddr=2200+Pump+Road+Suite+220,+Richmond,+VA+23233&amp;geocode=16698132703634901036,37.622358,-77.607256&amp;ei=_U-ATK6GLJTtngfRwLTqAQ&amp;ved=0CBQQngIwAA&amp;ll=37.622254,-77.60725&amp;spn=0.132022,0.110378&amp;z=13" target="_blank">For customized directions, click here to visit Google Maps.</a>		</p>
+		<img src="i/new-building-pump_road.jpg" width="219" height="164" /></div><!-- end #sidebar -->
+	  
+      
+            <div id="mainContent">
+
+                  <img src="i/Kirsten-Wolcott.jpg" /><div id="mainContentLeft">
+                  <div id="mainContentLeftPad">			
+		
+		<h1>Contact Us</h1>
+		<p>Thank you for your interest.<br />Please fill out the form below or <a href="mailto:info%40petersonnutrition%2ecom">send us an email at info@petersonnutrition.com</a>.</p><br />
+		
+
+
+    <h3>Client forms for first appointment</h3>
+			<p>Print, fill out, sign and email the "Client Information Form" before your appointment to <a href="mailto:info@petersonnutrition.com">info@petersonnutrition.com</a></p>
+			<ul class="pdf">
+				<li><a href="ClientInformationForm_2017.pdf">Client Information Form</a></li>
+			</ul>
+
+                      <p class="hr">&nbsp;</p>
+                      <p class="tesaserq">Is nutrition counseling covered by my insurance? Can I pay via credit card?</p>
+                      <ul class="intlink">
+                        <li><a href="faq.html">Get answers to FAQs</a>                  </li>
+                        
+                        <!--Hiding "BUY SESSIONS BUTTON" </ul>
+                        <p class="hr">&nbsp;</p>
+<a href="pay/index.html"><img src="i/buy-sessions.gif" width="160" height="37" alt="buy sessions" /></a> 
+                      <p class="hr">&nbsp;</p> -->
+
+                      <h3>Click below to pay bill online before appointment </h3>
+                      <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="XUYXY5T8VFUAU">
+<input type="image" src="http://www.petersonnutrition.com/pay/pay.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+                  </div>
+                    <!-- end #mainContentRightPad -->              
+                  </div><!-- end #mainContentRight -->
+                  <br class="clearfloat" />
+
+              </div><!-- end #mainContent -->
+              <br class="clearfloat" />
+        
+     </div><!-- end #content -->
+
+
+</div><!-- end #container -->
+
+	<div id="footer">
+	  <div id="footlft">&copy;&nbsp;<SCRIPT>
+<!--
+var year=new Date();
+year=year.getYear();
+if (year<1900) year+=1900;
+document.write(year);
+//-->
+</SCRIPT>&nbsp;Peterson Nutrition &amp; Fitness  |  <a href="index.html">Home</a> | <a href="about.html">About</a> | <a href="services.html">Services</a> | <a href="news.html">News</a> | <a href="resources.html">Resources</a> | <a href="contact.php">Contact</a></div>
+      <div id="footrt">Follow: <a href="http://www.facebook.com/pages/Peterson-Nutrition-Fitness/193558414029183"><img src="i/tiny_facebook.gif" alt="follow on facebook" /></a><a href="https://twitter.com/Elisabeth_Pete"><img src="i/tiny_twitter.gif" alt="follow on twitter" /></a><a href="https://instagram.com/stayhomeandeatwithme"><img src="i/tiny_insta.png" style="heigt:18px;width:16px;margin-left:3px" alt="follow on instagram" /></a></div>
+      <br class="clearfloat" />
+  </div><!-- end #footer -->
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-9692751-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+</body>
+</html>
