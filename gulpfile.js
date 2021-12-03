9c6d2297180e9c0ccaf5f9140e35c8de72e280f6
Erik Zettersten <erik@zettersten.com>
2021-12-03 16:22:07 -0500

    no env

--- gulpfile.js
+++ gulpfile.js
@@ -7,50 +7,50 @@ var include = require("gulp-html-tag-include");
 var clean = require("gulp-clean");
 
 gulp.task("dev", function() {
-    gulp.src("./dist")
-        .pipe(
-            webserver({
-                livereload: true,
-                open: true
-            })
-        );
+  gulp.src("./dist").pipe(
+    webserver({
+      livereload: true,
+      open: true
+    })
+  );
 });
 
 gulp.task("html-include", function() {
-    return gulp.src("./src/**/*.html")
-        .pipe(include())
-        .pipe(gulp.dest("./dist/"));
+  return gulp.src("./src/**/*.html").pipe(include()).pipe(gulp.dest("./dist/"));
 });
 
 gulp.task("build", function() {
-    return gulp.src("./src/**/!(*.html)")
-        .pipe(gulp.dest("./dist/"));
+  return gulp.src("./src/**/!(*.html)").pipe(gulp.dest("./dist/"));
 });
 
 gulp.task("clean", function() {
-    return gulp.src("./dist", { read: false })
-        .pipe(clean());
+  return gulp.src("./dist", { read: false }).pipe(clean());
 });
 
-gulp.task("default", gulp.parallel("html-include", "build", "dev", function() {
+gulp.task(
+  "default",
+  gulp.parallel("html-include", "build", "dev", function() {
     gulp.watch("./src/**/*.html", gulp.series("html-include"));
     gulp.watch("./src/**/!(*.html)", gulp.series("build"));
-}));
+  })
+);
 
-gulp.task("deploy", gulp.series("clean", "html-include", "build", function() {
-
-    env({ file: ".env.json" });
+gulp.task(
+  "deploy",
+  gulp.series("clean", "html-include", "build", function() {
+    // env({ file: ".env.json" });
 
     var conn = ftp.create({
-        host: process.env.FTP_HOST,
-        user: process.env.FTP_USER,
-        password: process.env.FTP_PASSWORD,
-        parallel: 2,
-        log: gutil.log
+      host: process.env.FTP_HOST,
+      user: process.env.FTP_USER,
+      password: process.env.FTP_PASSWORD,
+      parallel: 2,
+      log: gutil.log
     });
 
-    return gulp.src("./dist/**/*", { base: "./dist", buffer: false })
-        .pipe(conn.newer("/dist")) // only upload newer files
-        .pipe(conn.dest("/public_html"));
-
-}));
+    return gulp
+      .src("./dist/**/*", { base: "./dist", buffer: false })
+      .pipe(conn.newer("/dist")) // only upload newer files
+      .pipe(conn.dest("/public_html"));
+  })
+);
